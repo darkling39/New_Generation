8de51c885657dda533c4732767ed77e33a42fcee
Max <axioma162@gmail.com>
2021-12-11 17:15:48 +0200

    Lesson 1 task 3

---
+++ Lesson_1/Task 3/L1T3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 2;
+    int arr [] = {a = 2, b =5};
+    for (int i = 0; i <2; i++)
+    cout << arr[i] << " ";
+    return 0;
+}
