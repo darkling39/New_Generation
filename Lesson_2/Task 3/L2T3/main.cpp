e54e89019b433e54897081b0e3bbc0a130b9120d
Max <axioma162@gmail.com>
2021-12-15 23:47:06 +0200

    Lesson 2 Task 3

---
+++ Lesson_2/Task 3/L2T3/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Size;
+    cin >> Size;
+    for(int i = 0; i < Size; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+
+    for(int i =0; i < Size-2; i ++)
+    {
+        cout << "*" ;
+        for(int t =0; t < Size-2; t++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        cout << endl;
+    }
+     for(int i = 0; i < Size; i++)
+    {
+        cout << "*";
+    }
+    return 0;
+}
