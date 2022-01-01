bb879b89163bb5b972cfbe3cd65e2aa24a494f0e
Max <axioma162@gmail.com>
2022-01-01 19:16:32 +0200

    Lesson 2 task 2

--- Lesson_2/Task 2/L2T2/main.cpp
+++ Lesson_2/Task 2/L2T2/main.cpp
@@ -8,26 +8,25 @@ int main()
     int hash, def, rem1, rem2, sum = 0, last, first;
     cin >> hash;
     def = hash;
-    last = def%10;
-    
+    last = def % 10;
     while (def != 0)
     {
         rem1 = def % 10;
-        rem2 = (def/10) % 10;
+        rem2 = (def / 10) % 10;
         if (rem1 == rem2)
         {
             sum = sum + rem1;
         }
         def = def / 10;
     }
-        if (rem2 == 0)
-        {
-            first = rem1;
-        }
-        if (first = last)
-        {
-            sum = sum + first;
-        }
+    if (rem2 == 0)
+    {
+        first = rem1;
+    }
+    if (first = last)
+    {
+        sum = sum + first;
+    }
     cout << sum;
     return 0;
 }
