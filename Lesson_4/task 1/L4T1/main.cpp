445abb3ff0be567c976bd52b659e86ad50ced8ba
Max <axioma162@gmail.com>
2022-01-02 23:31:11 +0200

    Lesson 4 task 1

---
+++ Lesson_4/task 1/L4T1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int counter = 0;
+    string arr;
+    getline(cin, arr);
+
+    for(int i=1;i<arr.size();i++)
+    {
+        if(arr[i] == 33 || arr[i] == 34 || (arr[i] >= 44 && arr[i] <= 46) || arr[i] == 58 || arr[i] == 59 || arr[i] == 63)
+            counter++;
+    }
+
+    cout << counter << endl;
+
+    /*int arrSize = 20, cnt = 0;
+    char arr[arrSize];
+    for(int i = 0; i < arrSize; i++)
+    {
+        cin >> arr[i];
+        if(arr[i] == 33 || arr[i] == 34 || (arr[i] >= 44 && arr[i] <= 46) || arr[i] == 58 || arr[i] == 59 || arr[i] == 63)
+        {
+            cnt++;
+        }
+        if(arr[i] == 0)
+            break;
+    }
+    cout << endl;
+    cout << cnt;*/
+    return 0;
+}
