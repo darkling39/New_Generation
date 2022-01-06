8038d10ca5147e058af97f82b78449da892b4e99
Max <axioma162@gmail.com>
2022-01-06 22:11:55 +0200

    Lesson 4 task 2

--- Lesson_4/task 2/L4T2/main.cpp
+++ Lesson_4/task 2/L4T2/main.cpp
@@ -10,17 +10,18 @@ int main()
   cin.getline(str1, 256, ';');
   cout << endl;
   cin.getline(str2, 256, ';');
-
-
-    /*char str1[256], str2[256];
-    cin.getline(str1,);
-    getline(cin, str2);
-    for(int i = 0; i < str.size(); i++)
-    {
-        if(str1[i] == str2[i])
-            cout << "you have 2 same strings";
-        else
-            cout << "there are some differences";
-    }*/
+  for(int i = 0; i < 256; i++)
+  {
+      if(str1[i] == str2[i])
+      {
+          cout << "hi" << " ";
+          break;
+      }
+      else
+      {
+          cout << "Not hi" << " ";
+          break;
+      }
+  }
     return 0;
 }
